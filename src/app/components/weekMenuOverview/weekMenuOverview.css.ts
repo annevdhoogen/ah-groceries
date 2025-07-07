9070bd05f5296f30218dd2c30b59470a3b1d4e14
Anne van den Hoogen <vdhoogen.anne@gmail.com>
2025-07-07 11:41:36 +0200

    Fix layout on mobile for weekmenu

--- src/app/components/weekMenuOverview/weekMenuOverview.css.ts
+++ src/app/components/weekMenuOverview/weekMenuOverview.css.ts
@@ -11,7 +11,12 @@ export const list = style({
   listStyleType: "none",
   padding: 0,
   display: "grid",
-  gridTemplateColumns: "1fr 1fr",
+  gridTemplateColumns: "1fr",
   gap: "16px",
   gridArea: "recipes",
+  "@media": {
+    "screen and (min-width: 768px)": {
+      gridTemplateColumns: "1fr 1fr",
+    },
+  },
 });
